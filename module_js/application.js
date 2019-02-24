291e0a5916f2bad4dc8755001c7d1f4c0539396f
catherine Beaudoin-Rheault <43348739+alia600@users.noreply.github.com>
2019-02-24 18:47:36 -0500

    suprimation des fichiers qui ne serons pas utilisés

--- module_js/application.js
+++ module_js/application.js
@@ -1,12 +1,8 @@
-
-import {contenuIntro} from './contenuIntro.js' // le contenu de l'intoduction */
-import {Introduction} from './Introduction.js' // 
 import {AnimLettre} from './AnimLettre.js' //
 
 
 /* l'élement de la page qui contiendra les éléments créés dynamiquement */
-let elmHeader = document.querySelector('.header')
-let intro = new Introduction(contenuIntro, elmHeader, animationLettre)
+let elmTitre = document.querySelector('.titre')
 
 // debutQuestionnaire()
 
@@ -14,8 +10,8 @@ function animationLettre()
 {
     /* Une fois que l'animation des mots est terminé la fonction animLettre s'exécutera */ 
 	console.log('debut animation lettre')
-	const lesLettres = 'Veille-technologique'
- 	let  monAnimLettre = new AnimLettre(lesLettres, elmHeader, finAnim)	
+	const lesLettres = 'Animation de lettre'
+ 	let  monAnimLettre = new AnimLettre(lesLettres, elmTitre, finAnim)	
 }
 
 
